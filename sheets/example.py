633daca33f283ba7e1e15a71cccdd123f1df8043
chrislarabee9 <chris.larabee9@gmail.com>
2020-01-01 17:15:10 -0600

    Added an example sheets module and configured the gitignore to ignore actual sheets modules.

---
+++ sheets/example.py
@@ -0,0 +1,8 @@
+from lib.schema import Schema
+
+SHEET_ID = 'The string that comes between ' \
+           'https://docs.google.com/spreadsheets/d/ and /edit#gid='
+
+TABS = {
+    'tab_name': ['A*:C', 1, 'target_directory', Schema()]
+}
